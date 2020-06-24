e6c8de4a96cfb4b0e9c9bfcab7e6ef3ac7d9581e
vibha-i1 <63826712+vibha-i1@users.noreply.github.com>
2020-06-23 20:06:59 -0500

    Add files via upload

---
+++ build/precache-manifest.2fba9eb31620e057620b39453ed74bdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9ca1ca821568d3ed013db352695b5f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "9f46dde7094b91c11686",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "630e97bb68e59561ac4c",
+    "url": "/robofriends/static/css/main.6d815f62.chunk.css"
+  },
+  {
+    "revision": "9f46dde7094b91c11686",
+    "url": "/robofriends/static/js/2.f97d5952.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f97d5952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630e97bb68e59561ac4c",
+    "url": "/robofriends/static/js/main.30114eb6.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
